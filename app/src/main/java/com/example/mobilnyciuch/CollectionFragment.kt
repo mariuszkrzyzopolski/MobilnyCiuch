1040efc43067c0a7fc5a348b6c91db4213e90334
mariuszkrzyzopolski <krzyzopolski.mariusz@gmail.com>
2023-05-24 22:46:27 +0200

    scaling slider

--- app/src/main/java/com/example/mobilnyciuch/CollectionFragment.kt
+++ app/src/main/java/com/example/mobilnyciuch/CollectionFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.Fragment
 import com.example.mobilnyciuch.databinding.FragmentCollectionBinding
 import org.imaginativeworld.whynotimagecarousel.ImageCarousel
@@ -102,10 +103,14 @@ class CollectionFragment : Fragment() {
         )
 
         carouselUp.addData(listUp)
-        carouselUp.autoWidthFixing = true
-        carouselUp.carouselType = CarouselType.SHOWCASE
         carouselLow.addData(listlow)
         carouselFoot.addData(listfoot)
+
+        view.findViewById<Button>(R.id.button_collection).setOnClickListener {
+            var navRegister = activity as FragmentNawigation
+            navRegister.navigateFrag(MenuFragment(), false)
+        }
+
         return view
     }
 
